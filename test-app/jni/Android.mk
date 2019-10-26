3d497d48d8c07c243e933ab418a88f3f5257b23b
Maxim Makhun <maxim.makhun@gmail.com>
2019-10-26 18:59:31 +0200

    Add libraries to repo.

--- test-app/jni/Android.mk
+++ test-app/jni/Android.mk
@@ -22,29 +22,3 @@ LOCAL_SRC_FILES := ../lib/libcurl.so
 LOCAL_SHARED_LIBRARIES := ssl
 LOCAL_EXPORT_C_INCLUDES := include
 include $(PREBUILT_SHARED_LIBRARY)
-
-
-# LOCAL_PATH := $(call my-dir)
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := test.out
-# LOCAL_SRC_FILES := test.cpp
-# LOCAL_STATIC_LIBRARIES := curl
-# include $(BUILD_EXECUTABLE)	
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := crypto
-# LOCAL_SRC_FILES := ../lib/libcrypto.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := ssl
-# LOCAL_SRC_FILES := ../lib/libssl.a
-# LOCAL_STATIC_LIBRARIES := crypto
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := curl
-# LOCAL_SRC_FILES := ../lib/libcurl.a
-# LOCAL_STATIC_LIBRARIES := ssl
-# LOCAL_EXPORT_C_INCLUDES := include
-# include $(PREBUILT_STATIC_LIBRARY)
